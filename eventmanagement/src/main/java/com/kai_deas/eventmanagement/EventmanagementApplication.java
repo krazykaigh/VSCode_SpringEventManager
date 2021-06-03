3563cc8232e6326cf39590e74a2bc1c3c236eabc
Kaigh Taylor <coders@kai-deas.com>
2021-06-03 14:33:53 -0400

    VSCode_SEM | Maven dependency updates

--- eventmanagement/src/main/java/com/kai_deas/eventmanagement/EventmanagementApplication.java
+++ eventmanagement/src/main/java/com/kai_deas/eventmanagement/EventmanagementApplication.java
@@ -2,8 +2,11 @@ package com.kai_deas.eventmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {EventmanagementApplication.class, Jsr310Converters.class})
 public class EventmanagementApplication {
 
 	public static void main(String[] args) {
