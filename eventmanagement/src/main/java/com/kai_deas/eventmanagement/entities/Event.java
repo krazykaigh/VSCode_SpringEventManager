0a78b76a36bb2a7910b775690fdb9721f87d3c5c
Kaigh Taylor <coders@kai-deas.com>
2021-06-03 21:21:52 -0400

    VSCode_SEM | Fixed Venue Annotation

--- eventmanagement/src/main/java/com/kai_deas/eventmanagement/entities/Event.java
+++ eventmanagement/src/main/java/com/kai_deas/eventmanagement/entities/Event.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import org.springframework.data.rest.core.annotation.RestResource;
+
 @Entity
 public class Event extends AbstractEntity {
 
@@ -27,6 +29,7 @@ public class Event extends AbstractEntity {
     @OneToMany(mappedBy = "event", fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Participant> participants;
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @RestResource(exported = false)
     private Venue venue;
 
     public String getName() {
